6fb690b47ee2c26c7431460b646069d8610d5a7e
zhierchen0621 <91171547+zhierchen0621@users.noreply.github.com>
2022-04-07 17:27:17 +0800

    Add files via upload

---
+++ week07/week07-3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+char line[2000][80];
+int compare(const void *p1,const void *p2)
+{
+	return strcmp((char*)p1,(char*)p2);
+}
+int main()
+{
+	int N;
+	scanf("%d\n",&N);
+
+	for(int i=0;i<N;i++){
+		scanf("%s",line[i]);
+		char others[80];
+		gets( others );
+	}
+
+	qsort(line,N,80,compare);
+
+	for(int i=0;i<N;i++){
+		printf("%s\n",line[i]);
+	}
+	return 0;
+}
